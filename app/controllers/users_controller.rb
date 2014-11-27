dd47a1d016a5e1d2a6cc67259a4b1db1bc61e36b
Laura Trigeiro <lauratrigeiro@gmail.com>
2014-11-27 10:50:56 -0800

    Add search form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def search
-  	@users = User.paginate(page: params[:page])
+#  	@users = User.paginate(page: params[:page])
+	@users = User.all
+	@users = if params[:search_by] == 'city'
+      			@users.where("city ILIKE ?", "%#{params[:query]}%")
+      		elsif params[:search_by] == 'state'
+      			@users.where("state ILIKE ?", "%#{params[:query]}%")
+      		elsif params[:search_by] == 'country'
+      			@users.where("country ILIKE ?", "%#{params[:query]}%")
+  			end
+ 	@users = @users.paginate(page: params[:page]) if  @users && @users.any?
   end
 end
+
